01d18442e46028f21685730c16bde021a5e14664
Rayan94 <rayan.aittayeb@gmail.com>
2017-03-06 16:21:21 +0100

    New file
    Added diamond.js

---
+++ assets/js/diamond.js
@@ -0,0 +1,13 @@
+var diamond = function(){
+  
+  this.posX;
+  this.posY;
+  this.bonus;
+  this.color;
+  this.taille;
+  this.duree;
+  this.createElement = function(){};
+  this.afficher = function(){};
+  this.enlever = function(){};
+  this.genererBonus = function(){};
+}
