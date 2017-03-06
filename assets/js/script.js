53356d51034aa7fcfdc92f10b810b765c6bb5a6d
AnnaBmnn <annabaumann@laposte.net>
2017-03-06 16:17:15 +0100

    add snake class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-console.log('Yo');
+var snake = function(){
+  
+  this.posX;
+  this.posY;
+  this.point;
+  this.vitesse;
+  this.couleur;
+  this.direction;
+  this.keyCode;
+  this.queue;
+  this.bonus;
+  this.create = function(){};
+  this.afficher = function(){};
+  this.lancer = function(){};
+  this.changerdirection = function(){};
+  this.isCollision = function(){};
+  this.bouger = function(){};
+  this.afficherPoint = function(){};
+  this.kill = function(){};
+  
+  
+  
+}
